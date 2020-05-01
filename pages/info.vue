3fca09a4165ef231dc1b73a31d9357684ec87f97
Iddar Olivares <iddar@dbug.mx>
2020-05-01 18:53:35 -0500

    primer commit del proyecto del flisol 2020

---
+++ pages/info.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="conten">
+    <h1>Info page</h1>
+    <p>Esto es una pagina de informacion</p>
+  </div>
+</template>
+
+<style scoped>
+.conten {
+  max-width: 400px;
+  margin: 5rem auto;
+}
+</style>
