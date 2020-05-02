d266d532965e309e54c4d0aa9f81195a3edadf94
Iddar Olivares <iddar@dbug.mx>
2020-05-01 19:02:53 -0500

    agregamos una caja en la pagina /info

--- pages/info.vue
+++ pages/info.vue
@@ -2,6 +2,10 @@
   <div class="conten">
     <h1>Info page</h1>
     <p>Esto es una pagina de informacion</p>
+
+    <div class="box">
+      <p>Esto es una prueba de CI/CD en vivo para los chicos del itver</p>
+    </div>
   </div>
 </template>
 
@@ -10,4 +14,16 @@
   max-width: 400px;
   margin: 5rem auto;
 }
+
+h1 {
+  color: gray;
+}
+
+.box {
+  border: solid 1px gray;
+  padding: 1rem;
+  margin: 1.5rem;
+  border-radius: 10px;
+}
 </style>
+
